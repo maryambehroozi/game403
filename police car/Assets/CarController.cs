82664b31e9eee168608fd2ae4eff15dfeae43dae
Arefalj <aref.alj1381@gmail.com>
2024-11-25 16:49:31 +0330

    added siren and light and changed car model

--- police car/Assets/CarController.cs
+++ police car/Assets/CarController.cs
@@ -13,6 +13,8 @@ public class CarController : MonoBehaviour
     private float currentSteerAngle;
     private float currentbreakForce;
     private bool isBreaking;
+    private bool isFrontLightOn;
+    private bool isSirenOn;
 
     [SerializeField] private float motorForce;
     [SerializeField] private float breakForce;
@@ -28,17 +30,55 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
-    private void FixedUpdate()
+    [SerializeField] private GameObject frontLight;
+    [SerializeField] private Light redTopLight;
+    [SerializeField] private Light blueTopLight;
+
+    private float blinkRate = 2.0f; 
+    private float currentTime = 0.0f;
+    private float maxSirenLight = 3.0f;
+    private float minSirenLight = 1.0f;
+    private float defaultSirenLight = 1.0f;
+
+    private void Update()
     {
+        
         GetInput();
+        TopHandleLight();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+        HandleLight();
     }
 
+    private void TopHandleLight()
+    {
+        currentTime += Time.fixedDeltaTime;
+        float cyclePosition = Mathf.PingPong(currentTime, blinkRate) / blinkRate;
+        redTopLight.intensity = Mathf.Lerp(0, defaultSirenLight, cyclePosition);
+        blueTopLight.intensity = Mathf.Lerp(defaultSirenLight, 0, cyclePosition);
+    }
 
     private void GetInput()
     {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            isFrontLightOn = !isFrontLightOn;
+        }
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            isSirenOn = !isSirenOn;
+            if(isSirenOn)
+            {
+                defaultSirenLight = maxSirenLight;
+                gameObject.GetComponent<AudioSource>().Play();
+            }
+            else
+            {
+                defaultSirenLight = minSirenLight;
+                gameObject.GetComponent<AudioSource>().Stop();
+            }
+        }
         horizontalInput = Input.GetAxis(HORIZONTAL);
         verticalInput = Input.GetAxis(VERTICAL);
         isBreaking = Input.GetKey(KeyCode.Space);
@@ -50,6 +90,18 @@ public class CarController : MonoBehaviour
         frontRightWheelCollider.motorTorque = verticalInput * motorForce;
         currentbreakForce = isBreaking ? breakForce : 0f;
         ApplyBreaking();       
+    }
+
+    private void HandleLight()
+    {
+        if(isFrontLightOn)
+        {
+            frontLight.SetActive(true);
+        }
+        else
+        {
+            frontLight.SetActive(false);
+        }
     }
 
     private void ApplyBreaking()
@@ -71,8 +123,8 @@ public class CarController : MonoBehaviour
     {
         UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
         UpdateSingleWheel(frontRightWheelCollider, frontRightWheeTransform);
-        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
-        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        // UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        // UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
     }
 
     private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
